47ee2d71bd339e36e2ad07e4c797cd08bdb3786d
marobew <boramwe.dev@gmail.com>
2020-09-29 03:25:59 +0900

    MongoDB Comment Model Modeling

---
+++ server/models/comment.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const CommentSchema = new mongoose.Schema({
+  contents: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: moment().format("YYYY-MM-DD hh:mm:ss"),
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post",
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  createToName: { type: String },
+});
+
+const Comment = mongoose.model('commment', CommentSchema);
+
+export default Comment;
