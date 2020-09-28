cab4f6154cc5f07cddcb3fa9158d26b611ea88f4
marobew <boramwe.dev@gmail.com>
2020-09-29 03:08:57 +0900

    MongoDB User Model Modeling

---
+++ server/models/user.js
@@ -0,0 +1,50 @@
+import mongoose from 'moongoose';
+import moment from 'moment';
+
+//Create Schema
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	role: {
+		type: String,
+		enum: ["Main", "Sub", "User"],
+		default: "User",
+	},
+	register_date: {
+		type: Date,
+		default: moment.defaultFormat("YYYY-MM-DD hh:mm:ss")
+	},
+	comments: [
+		{
+			post_id: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "posts",
+			},
+			comment_id: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "comments",
+			},
+		},
+	],
+	post: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "posts",
+		},
+	],
+});
+
+const User = mongoose.model("user", UserSchema);
+
+export default User;
