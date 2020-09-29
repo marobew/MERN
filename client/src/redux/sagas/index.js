3069934ed93035aea8a5bc81c3452cece19ffae8
marobew <boramwe.dev@gmail.com>
2020-09-29 22:00:55 +0900

    redux 설정

---
+++ client/src/redux/sagas/index.js
@@ -0,0 +1,6 @@
+import { all } from 'redux-saga/effects';
+
+// 제너레이터 function*
+export default function* rootSaga() {
+  yield all([]);
+}
---
+++ client/src/store.js
@@ -0,0 +1,30 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+
+import createRootReducer from './redux/reducers/index';
+import rootSaga from './redux/sagas'
+
+export const history = createBrowserHistory();
+
+const createSagaMiddleware = createSagaMiddleware();
+
+// 초기 상태값을 빈 값으로 두기
+const initialState = {}
+
+const middlewares = [sagaMiddleware, routerMiddleware(history)]
+const devtools = window.__EXTENSION__COMPOSE__
+
+const composeEnhancer = process.env.NODE_ENV === 'production' ? compose : devtools || compose;
+
+// store 생성
+const store = createStore(
+  createRootReducer(history),
+  initialState,
+  composeEnhancer(applyMiddleware(...middlewares))
+)
+// sagaMiddleware 작동
+sagaMiddleware.run(rootSage)
+
+export default store;
